80933153279fbd789a3e0f9452dc288c2e8e0ea9
Ricky Li <lishingchunricky@gmail.com>
2023-09-26 16:45:17 +0800

    update dataloader

--- Transfer-Learning-Library/tllib/vision/datasets/reid/__init__.py
+++ Transfer-Learning-Library/tllib/vision/datasets/reid/__init__.py
@@ -3,5 +3,6 @@ from .dukemtmc import DukeMTMC
 from .msmt17 import MSMT17
 from .personx import PersonX
 from .unreal import UnrealPerson
+from .cuhk03 import CUHK03
 
-__all__ = ['Market1501', 'DukeMTMC', 'MSMT17', 'PersonX', 'UnrealPerson']
+__all__ = ['Market1501', 'DukeMTMC', 'MSMT17', 'PersonX', 'UnrealPerson', 'CUHK03']
