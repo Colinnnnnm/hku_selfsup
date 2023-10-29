58ba0849e45ba0bbb4e8f612c8debafaf461c6fe
Jeff Chan <75520235+khcjeffchan@users.noreply.github.com>
2023-10-29 16:04:20 +0800

    Update make_model.py (add function print_train_parameter)

--- TransReID-SSL-lora/transreid_pytorch/model/make_model.py
+++ TransReID-SSL-lora/transreid_pytorch/model/make_model.py
@@ -461,6 +461,20 @@ __factory_T_type = {
     'vit_large_patch16_1024_sam': vit_large_patch16_1024_sam
 }
 
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param:.2f}"
+    )
+    
 def make_model(cfg, num_class, camera_num, view_num):
     if cfg.MODEL.NAME == 'transformer':
         if cfg.MODEL.JPM:
